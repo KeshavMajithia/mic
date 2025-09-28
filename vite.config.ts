8ac8ca30c1dcebaf7c1998211f97b8fd39546735
KeshavMajithia <65853673+KeshavMajithia@users.noreply.github.com>
2025-09-28 11:17:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/mic/' : '/',
+  // base: mode === 'production' ? '/mic/' : '/',  // Comment this out
+  base: '/', // Use root path for now
   server: {
     host: "0.0.0.0",
     port: 8080,
