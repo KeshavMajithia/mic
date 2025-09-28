6adc473d086ab131423a91a21a47c8b2703d3b3c
KeshavMajithia <65853673+KeshavMajithia@users.noreply.github.com>
2025-09-28 11:29:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/mic">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/rate-finder" element={<RateFinder />} />
